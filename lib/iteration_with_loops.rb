4902b342a449b9cc5a9db2b81fc86e9bcf01c72b
David Lee <david.lee.sl2634@yale.edu>
2020-05-15 14:41:08 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,9 +2,9 @@
 def find_min_in_nested_arrays(src)
   final_results = []
   counter = 0 
+  min = src[counter][0]
   while counter < src.count do 
     inner_counter = 0 
-    min = src[counter][0]
     while inner_counter < src[counter].count do 
       if src[counter][inner_counter] < min
        min = src[counter][inner_counter]
